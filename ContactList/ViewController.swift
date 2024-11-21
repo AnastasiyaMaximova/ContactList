f454d3f2b86a9d8945b7a2fff69623b7d3a9550a
AnastasyiaMaximova <rsk.maximova@bk.ru>
2024-11-21 19:48:15 +0300

    Initial Commit

---
+++ ContactList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactList
+//
+//  Created by Anastasya Maximova on 21.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
